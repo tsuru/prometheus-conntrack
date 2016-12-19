f8a1731e4a2216220efae712ab44a9559aeac5d9
André Carvalho <asantostc@gmail.com>
2016-12-19 13:10:11 -0200

    add benchmark test

--- collector_test.go
+++ collector_test.go
@@ -43,3 +43,50 @@ func (*S) TestCollector(c *check.C) {
 	c.Assert(lines[2], check.Equals, `container_connections{container_label_label1="val1",destination="192.168.50.4:2375",id="id",image="image",name="name",protocol="tcp",state="ESTABLISHED"} 2`)
 	c.Assert(lines[3], check.Equals, `container_connections{container_label_label1="val1",destination="192.168.50.5:2376",id="id",image="image",name="name",protocol="tcp",state="ESTABLISHED"} 1`)
 }
+
+func (s *S) BenchmarkCollector(c *check.C) {
+	collector := &ConntrackCollector{
+		containerLister: func() ([]*docker.Container, error) {
+			return []*docker.Container{
+				{
+					ID:              "id",
+					Name:            "name",
+					Config:          &docker.Config{Image: "image", Labels: map[string]string{"label1": "val1"}},
+					NetworkSettings: &docker.NetworkSettings{IPAddress: "10.10.1.2"},
+				},
+				{
+					ID:              "id2",
+					Name:            "name",
+					Config:          &docker.Config{Image: "image", Labels: map[string]string{"label1": "val1"}},
+					NetworkSettings: &docker.NetworkSettings{IPAddress: "10.10.1.3"},
+				},
+			}, nil
+		},
+		conntrack: func() ([]conn, error) {
+			return []conn{
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2375", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.3", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2374", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.6", DestinationPort: "2376", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.3", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2375", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2375", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.7", DestinationPort: "2376", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.3", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2374", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.6", DestinationPort: "2375", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.5", DestinationPort: "2376", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.3", SourcePort: "33404", DestinationIP: "192.168.50.5", DestinationPort: "2375", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.2", SourcePort: "33404", DestinationIP: "192.168.50.4", DestinationPort: "2374", State: "ESTABLISHED", Protocol: "tcp"},
+				{SourceIP: "10.10.1.1", SourcePort: "33404", DestinationIP: "192.168.50.6", DestinationPort: "2376", State: "ESTABLISHED", Protocol: "tcp"},
+			}, nil
+		},
+	}
+	ch := make(chan prometheus.Metric)
+	go func() {
+		for _ = range ch {
+
+		}
+	}()
+	for n := 0; n < c.N; n++ {
+		collector.Collect(ch)
+	}
+	close(ch)
+}
